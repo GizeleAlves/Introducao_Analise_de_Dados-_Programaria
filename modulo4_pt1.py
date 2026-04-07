7df94918710c2c29070ea933c9de65fdba19a4be
GizeleAlves <gizele0alves@gmail.com>
2026-04-06 21:12:18 -0300

    Módulo 4 - Introdução

---
+++ modulo4_pt1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Módulo 4 - Estátistica Básica
+
+"""
+# %%
+import numpy as np
+
+lista_idades = [26, 30, 32, 22, 26, 35, 40, 20, 43, 31, 23, 100]
+
+np.sum(lista_idades) #soma dos valores das idades
+
+len(lista_idades) #quantidade de elementos na lista
+
+media = np.mean(lista_idades) #média das idades
+
+lista_idades.sort() #ordenando a lista em ordem crescente
+
+mediana = np.median(lista_idades) #mediana das idades
+
+# %%
+
+
