58587957c141996bd24b156e97dc438e64f17197
GizeleAlves <gizele0alves@gmail.com>
2026-04-07 09:42:12 -0300

    Modulo 3 - Média, moda e mediana.

--- modulo4_pt1.py
+++ modulo4_pt1.py
@@ -5,6 +5,8 @@ Módulo 4 - Estátistica Básica
 """
 # %%
 import numpy as np
+import pandas as pd
+
 
 lista_idades = [26, 30, 32, 22, 26, 35, 40, 20, 43, 31, 23, 100]
 
@@ -18,6 +20,43 @@ lista_idades.sort() #ordenando a lista em ordem crescente
 
 mediana = np.median(lista_idades) #mediana das idades
 
+# %% 
+"""
+Aplicando estatística em um dataframe
+
+"""
+
+dados = pd.read_excel("planilha_modulo3.xlsx")
+
+dados['IDADE'].mean() #Média da coluna "Idade"
+
+dados['IDADE'].median() #Mediana da coluna "Idade"
+
+dados['IDADE'].mode() #Moda da coluna "Idade" (Número que mais se repete)
+
+dados['IDADE'].std() #Desvio padrão da coluna "Idade"
+
+dados['IDADE'].min() #Idade mínima
+
+dados['IDADE'].max() #Idade máxima
+# %%
+#Média de Idade do Sexo feminino
+
+dados[dados['GENERO']=='Feminino']['IDADE'].mean()
+
+#Média de Idade do Sexo masculino
+
+dados[dados['GENERO']=='Masculino']['IDADE'].mean()
 # %%
 
+#Média de Salário por sexo
+
+#Média de Salário do Sexo feminino
+
+dados[dados['GENERO']=='Feminino']['SALARIO'].mean()
+
+#Média de Salário do Sexo masculino
+
+dados[dados['GENERO']=='Masculino']['SALARIO'].mean()
+# %%
 
---
+++ modulo4_pt2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Módulo 4 - Estátistica Básica Parte 1
+
+"""
+# %%
+import numpy as np
+import pandas as pd
+
+dados = pd.read_excel("planilha_modulo3.xlsx")
+# %%
+
+
+
